01878b72d4ef29b21dabe531f9bca48f5cb13ec6
cheeleong <klrkdekira@gmail.com>
2013-11-17 19:29:07 +0800

    Replaced bootstrap, modernizr, jquery to third party cdn provider. Removed bootstrap, modernizr, jquery files. Updated pagination for bill index.

--- billwatcher/static/js/main.js
+++ billwatcher/static/js/main.js
@@ -1,1 +1,14 @@
+$(function () {
+    var pager = $("div#pager");
+    var ul = $('<ul class="pagination" />');
+    $("div#pager *").each(function (k, v) {
+	console.log(k, v);
+        var li = $("<li/>");
+        li.html($(this));
+        ul.append(li);
+    }).promise()
+        .done(function () {
+            pager.html(ul);
+        });
+});
 
